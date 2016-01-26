8eae71aa092bf020588a0c5be3a6e8ca8676539a
alexizvely <alexizvely@gmail.com>
2016-01-26 15:27:57 +0200

    Initial Commit

---
+++ HappyGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  HappyGame
+//
+
+//  Copyright (c) 2016 veli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
